179d9cd90d3da5bc42a15ffbf06841c37c276ae1
Rangecr <misaifullah2003@gmail.com>
2024-11-01 23:35:41 +0800

    Initial commit

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->registerPolicies();
+
+        Gate::define('gate-name', function ($user) {
+            return $user->isAdmin();
+        });
+    }
+}
