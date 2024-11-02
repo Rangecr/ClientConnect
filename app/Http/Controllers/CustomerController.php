933688e9fa94f737ad4586f8c64530189dc9e31b
Rangecr <misaifullah2003@gmail.com>
2024-11-02 19:24:38 +0800

    Adding controllers

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+use App\Models\Customer;
+use App\Models\Log;
+use App\Models\Ticket;
+
+class CustomerController extends Controller
+{
+    //
+}
